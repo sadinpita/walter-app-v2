0bd5590d176ec21d695899f2ed7bfacb291c7ab8
sadin97 <sadinpita_97@windowslive.com>
2019-08-14 10:14:13 +0200

    implementing pipe search filter

--- src/app/filters/search.pipe.ts
+++ src/app/filters/search.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { find, result } from 'lodash';
 
 @Pipe({
   name: 'search'
 })
+
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any, args: any[]) {
+    if (args[1]) {
+      return value.filter(function (el: any) {
+        return el[args[0]].toLowerCase().indexOf(args[1].toLowerCase()) > -1;
+      });
+    }
+    return value;
   }
 
 }
