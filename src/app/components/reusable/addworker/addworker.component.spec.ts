f54dae90048052745e498ae094922f8e0b3a6fe1
sadin97 <sadinpita_97@windowslive.com>
2019-08-16 14:53:07 +0200

    changed add worker component name

--- src/app/components/reusable/addworker/addworker.component.spec.ts
+++ src/app/components/reusable/addworker/addworker.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddworkerComponent } from './addworker.component';
+import { AddWorkerDialog } from './addworker.component';
 
 describe('AddworkerComponent', () => {
-  let component: AddworkerComponent;
-  let fixture: ComponentFixture<AddworkerComponent>;
+  let component: AddWorkerDialog;
+  let fixture: ComponentFixture<AddWorkerDialog>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddworkerComponent ]
+      declarations: [ AddWorkerDialog ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AddworkerComponent);
+    fixture = TestBed.createComponent(AddWorkerDialog);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
